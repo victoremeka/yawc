ec799f2182b783a4cb43b6338211be0977b0791a
Victor Emeka <98593069+victoremeka@users.noreply.github.com>
2025-12-23 01:44:01 +0100

    added -c cli option

--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+)
 
+func check(err error){
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func CalculateNumberOfBytes(path string) int {
+	absPath, err := filepath.Abs(path)
+	check(err)
+
+	f, err := os.ReadFile(absPath)
+	check(err)
+
+	return len(f)
+}
+
+func CalculateNumberOfLines(path string) int {
+
+}
+
+
+func main() {
+	args := os.Args
+
+	path := args[2]
+	switch cliOption := args[1]; cliOption {
+		case "-c":
+			nob := CalculateNumberOfBytes(path)
+			fmt.Printf("%d %s\n", nob, path)
+	}
 }
