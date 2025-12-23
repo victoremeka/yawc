00384e8802f379d42d86a55d122b6b13987bb108
Victor Emeka <98593069+victoremeka@users.noreply.github.com>
2025-12-23 09:10:16 +0100

    added -w cli option

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
 	"path/filepath"
-	"bufio"
 )
 
-func check(err error){
+func check(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -37,7 +37,23 @@ func CalculateNumberOfLines(path string) int {
 	return count
 }
 
+func CalculateNumberOfWords(path string) int {
+	f, err := os.Open(path)
+	check(err)
 
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	scanner.Split(bufio.ScanWords)
+
+	count := 0
+	for scanner.Scan() {
+		count++
+	}
+
+	return count
+}
 
 func main() {
 	args := os.Args
@@ -45,11 +61,13 @@ func main() {
 	absPath, err := filepath.Abs(path)
 	var res int
 	switch cliOption := args[1]; cliOption {
-		case "-c":
-			res = CalculateNumberOfBytes(absPath)
-			check(err)
-		case "-l":
-			res = CalculateNumberOfLines(absPath)
+	case "-c":
+		res = CalculateNumberOfBytes(absPath)
+		check(err)
+	case "-l":
+		res = CalculateNumberOfLines(absPath)
+	case "-w":
+		res = CalculateNumberOfWords(absPath)
 	}
 	fmt.Printf("%d %s\n", res, path)
 }
